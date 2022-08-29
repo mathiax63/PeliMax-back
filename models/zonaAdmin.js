ba9dfba19039a69145387dc9ae97b84986c4150d
mathiax63 <mathyoyo@hotmail.es>
2022-08-29 15:55:07 -0300

    subo cosas

---
+++ models/zonaAdmin.js
@@ -0,0 +1,69 @@
+let pool = require("./bd");
+
+
+async function todasLasPeliculas(){
+    
+        let query ="select * from peliculas";
+        let rows = await pool.query(query);
+        //console.log(rows)
+        return rows
+    
+}
+async function agregarProducto(obj){
+    try{
+        let query ="insert into peliculas set ?";
+        let rows = await pool.query(query, [obj])
+        return rows;
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+async function borrar(id){
+    let query = "delete from peliculas where id = ?";
+    let rows = await pool.query(query, [id]);
+    return rows
+}
+
+async function actualizar(id) {
+    let query = "select * from peliculas where id = ?"
+    let rows = await pool.query(query, [id]);
+    return rows[0]
+}
+
+async function modificarecho(obj, id){
+    try{
+        let query = "update peliculas set ? where id=?";
+        let rows = await pool.query(query, [obj, id]);
+        return rows;
+    }catch(error){
+        throw error
+    }
+}
+async function las5P(){
+    
+    let query ="select * from peliculas order by id desc limit 5 ";
+    let rows = await pool.query(query);
+    //console.log(rows)
+    return rows
+
+}
+async function las5PD(){
+    
+    let query ="select * from peliculas order by estrellas desc limit 5 ";
+    let rows = await pool.query(query);
+    //console.log(rows)
+    return rows
+
+}
+
+async function lasPDet(){
+    
+    let query ="select * from peliculas where id = ?";
+    let rows = await pool.query(query, [id]);
+    return rows[0]
+
+
+}
+
+module.exports = {todasLasPeliculas, agregarProducto, borrar, actualizar, modificarecho,las5P,las5PD,lasPDet}
---
+++ routes/api.js
@@ -0,0 +1,163 @@
+var express = require('express');
+var router = express.Router();
+let todasLasPeliculasModel= require("../models/zonaAdmin")
+const cloudinary = require("cloudinary").v2;
+let nodemailer = require("nodemailer")
+
+
+
+      router.post("/contacto", async (req,res) =>{
+        if (!req.body || !req.body.email) {
+          res.status(400).json ({
+            error: false
+          })}
+        const mail={
+          to:"mathyoyo@hotmail.es",
+          subject:"contacto web",
+          html: `${req.body.email} se contacto a traves de la web y quiera pedir la pelicula ${req.body.pedido}`
+        }
+        
+        const transport = nodemailer.createTransport({
+          host: process.env.SMTP_HOST,
+          port: process.env.SMTP_PORT,
+          auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+          }
+        })
+        await transport.sendMail(mail)
+        res.status(201).json({
+          error: false,
+          message: "mensaje enviado"
+        })
+      })
+      
+
+
+router.get('/contacto', async function(req, res, next) {
+
+    let titulosYSinopsis = await todasLasPeliculasModel.todasLasPeliculas()
+    
+    titulosYSinopsis = titulosYSinopsis.map(peliculas =>{
+      if(peliculas.imagenes){
+        const imagen = cloudinary.url(peliculas.imagenes,{
+         
+          crop:"fill"
+        });
+        return {
+          ...peliculas,
+          imagen
+        }
+      }else{
+        return {
+          ...peliculas,
+          imagen:" "
+        }
+      }
+    })
+    res.json(titulosYSinopsis)
+      });
+      router.get('/detalles', async function(req, res, next) {
+
+        let titulosYSinopsis = await todasLasPeliculasModel.lasPDet()
+        
+        titulosYSinopsis = titulosYSinopsis.map(peliculas =>{
+          if(peliculas.imagenes){
+            const imagen = cloudinary.url(peliculas.imagenes,{
+          
+              crop:"fill"
+            });
+            return {
+              ...peliculas,
+              imagen
+            }
+          }else{
+            return {
+              ...peliculas,
+              imagen:" "
+            }
+          }
+        })
+        res.json(titulosYSinopsis)
+          });
+
+
+
+      router.get('/detalles/{id}', async function(req, res, next) {
+
+        let titulosYSinopsis = await todasLasPeliculasModel.lasPDet()
+        
+        titulosYSinopsis = titulosYSinopsis.map(peliculas =>{
+          if(peliculas.imagenes){
+            const imagen = cloudinary.url(peliculas.imagenes,{
+          
+              crop:"fill"
+            });
+            return {
+              ...peliculas,
+              imagen
+            }
+          }else{
+            return {
+              ...peliculas,
+              imagen:" "
+            }
+          }
+        })
+        res.json(titulosYSinopsis)
+          });
+          router.get('/b', async function(req, res, next) {
+
+            let titulosYSinopsis = await todasLasPeliculasModel.las5P()
+            
+            
+            titulosYSinopsis = titulosYSinopsis.map(peliculas =>{
+              if(peliculas.imagenes){
+                const imagen = cloudinary.url(peliculas.imagenes,{
+                 
+                  crop:"fill"
+                });
+                return {
+                  ...peliculas,
+                  imagen
+                }
+              }else{
+                return {
+                  ...peliculas,
+                  imagen:" "
+                }
+              }
+            })
+            res.json(titulosYSinopsis)
+              });
+              router.get('/a', async function(req, res, next) {
+
+                let titulosYSinopsiss = await todasLasPeliculasModel.las5PD()
+                
+                titulosYSinopsiss = titulosYSinopsiss.map(peliculas =>{
+                  if(peliculas.imagenes){
+                    const imagen = cloudinary.url(peliculas.imagenes,{                     
+                      crop:"fill"
+                    });
+                    return {
+                      ...peliculas,
+                      imagen
+                    }
+                  }else{
+                    return {
+                      ...peliculas,
+                      imagen:" "
+                    }
+                  }
+                })
+                res.json(titulosYSinopsiss)
+                  });
+        
+    
+
+
+
+
+
+    
+    module.exports = router
